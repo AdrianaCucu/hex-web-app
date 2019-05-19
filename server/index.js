88d01c0b12a1abb21b6124170f5ff9c0b19ec19e
Adriana <adrianacucu99@gmail.com>
2019-05-19 20:46:58 +0100

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({
+    logger: true
+  })
+  
+  // Declare a route
+  fastify.get('/', (request, reply) => {
+    reply.send({ hello: 'world' })
+  })
+  
+  // Run the server!
+  fastify.listen(3000, (err, address) => {
+    if (err) throw err
+    fastify.log.info(`server listening on ${address}`)
+  })
+  
